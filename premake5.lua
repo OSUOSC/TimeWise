bde86c66471f5692a4f8c2d2dc44c898bc8cacc1
BanceDev <lanceaborden@protonmail.com>
2024-10-05 15:43:26 -0400

    added windows build with visual studio

--- premake5.lua
+++ premake5.lua
@@ -1,49 +1,55 @@
 workspace("timewise")
-configurations({ "Debug", "Release" })
-platforms({ "Linux", "Windows", "MacOS" })
+   configurations({ "Debug", "Release" })
+   platforms({ "Linux", "Windows", "MacOS" })
 
 -- TimeWise project
 project("timewise")
-kind("WindowedApp")
-language("C++")
-targetdir("bin/%{cfg.buildcfg}/timewise")
+   kind("WindowedApp")
+   language("C++")
+   targetdir("bin/%{cfg.buildcfg}/timewise")
 
-includedirs({ "lib/imgui", "lib/imgui/backends" })
+   -- Common include directories
+   includedirs({ "lib/imgui", "lib/imgui/backends" })
 
-files({
-	"src/**.h",
-	"src/**.cpp",
-	"lib/imgui/*.cpp",
-	"lib/imgui/backends/imgui_impl_glfw.cpp",
-	"lib/imgui/backends/imgui_impl_opengl3.cpp",
-})
+   -- Common files
+   files({
+      "src/**.h",
+      "src/**.cpp",
+      "lib/imgui/*.cpp",
+      "lib/imgui/backends/imgui_impl_glfw.cpp",
+      "lib/imgui/backends/imgui_impl_opengl3.cpp",
+   })
 
-filter("configurations:Debug")
-defines({ "DEBUG" })
-symbols("On")
+   filter("configurations:Debug")
+      defines({ "DEBUG" })
+      symbols("On")
 
-filter("configurations:Release")
-defines({ "NDEBUG" })
-optimize("On")
+   filter("configurations:Release")
+      defines({ "NDEBUG" })
+      optimize("On")
 
-filter("system:linux")
-defines({ "LINUX" })
-buildoptions({ "-std=c++11", "-g", "-Wall", "-Wformat" })
-links({ "GL", "glfw" })
-linkoptions({ "`pkg-config --static --libs glfw3`" })
-includedirs({ "`pkg-config --cflags glfw3`" })
+   -- Linux-specific setup
+   filter("system:linux")
+      defines({ "LINUX" })
+      buildoptions({ "-std=c++11", "-g", "-Wall", "-Wformat" })
+      links({ "GL", "glfw" })
+      linkoptions({ "`pkg-config --static --libs glfw3`" })
+      includedirs({ "`pkg-config --cflags glfw3`" })
 
-filter("system:windows")
-defines({ "WINDOWS" })
-buildoptions({ "-std=c++11" })
-links({ "glfw3", "gdi32", "opengl32", "imm32" })
+   -- Windows-specific setup for Visual Studio
+   filter("system:windows")
+      defines({ "WINDOWS" })
+      buildoptions({ "/std:c++17" })  -- Visual Studio uses MSVC syntax for build options
+      links({ "glfw3", "gdi32", "opengl32", "imm32" })
+      includedirs({ "lib/imgui", "lib/imgui/backends", "src" }) -- Include source directories for Windows
 
-filter("system:macosx")
-defines({ "MACOS" })
-buildoptions({ "-std=c++11" })
-links({ "OpenGL.framework", "Cocoa.framework", "IOKit.framework", "CoreVideo.framework", "glfw" })
-includedirs({ "/usr/local/include", "/opt/local/include", "/opt/homebrew/include" })
-libdirs({ "/usr/local/lib", "/opt/local/lib", "/opt/homebrew/lib" })
+   -- macOS-specific setup
+   filter("system:macosx")
+      defines({ "MACOS" })
+      buildoptions({ "-std=c++11" })
+      links({ "OpenGL.framework", "Cocoa.framework", "IOKit.framework", "CoreVideo.framework", "glfw" })
+      includedirs({ "/usr/local/include", "/opt/local/include", "/opt/homebrew/include" })
+      libdirs({ "/usr/local/lib", "/opt/local/lib", "/opt/homebrew/lib" })
 
 -- Reset filters to avoid affecting other projects
 filter({})
