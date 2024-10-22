f648dd6788317d429975fd70923463c1e793ec6e
BanceDev <lanceaborden@protonmail.com>
2024-10-21 23:05:19 -0400

    added basic calendar panel with current month

--- src/calendar_panel.h
+++ src/calendar_panel.h
@@ -1,9 +1,14 @@
 #pragma once
+#include <string>
 
 class CalendarPanel {
 public:
     CalendarPanel();
     void draw();
 private:
-    
+    std::string getMonth();
+    std::string getYear();
+    int getStartDay();
+    int getDaysInMonth();
+    int getDaysInLastMonth();
 };
--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,7 @@
 #include "imgui.h"
 #include "imgui_impl_glfw.h"
 #include "imgui_impl_opengl3.h"
+#include "imgui_internal.h"
 #include <stdio.h>
 #define GL_SILENCE_DEPRECATION
 #if defined(IMGUI_IMPL_OPENGL_ES2)
@@ -70,7 +71,7 @@ int main(int, char**)
 #endif
 
     // Create window with graphics context
-    GLFWwindow* window = glfwCreateWindow(1280, 720, "Dear ImGui GLFW+OpenGL3 example", nullptr, nullptr);
+    GLFWwindow* window = glfwCreateWindow(1280, 720, "TimeWise", nullptr, nullptr);
     if (window == nullptr)
         return 1;
     glfwMakeContextCurrent(window);
@@ -86,10 +87,11 @@ int main(int, char**)
     io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;       // Enable Multi-Viewport / Platform Windows
     //io.ConfigViewportsNoAutoMerge = true;
     //io.ConfigViewportsNoTaskBarIcon = true;
+    io.Fonts->AddFontFromFileTTF("./res/ubuntu.ttf", 21.0f);
+    io.Fonts->AddFontFromFileTTF("./res/ubuntu.ttf", 32.0f);
 
     // Setup Dear ImGui style
     ImGui::StyleColorsDark();
-    //ImGui::StyleColorsLight();
 
     // When viewports are enabled we tweak WindowRounding/WindowBg so platform windows can look identical to regular ones.
     ImGuiStyle& style = ImGui::GetStyle();
