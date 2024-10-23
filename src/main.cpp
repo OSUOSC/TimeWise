b0a9057b9364d464789e33ecd086a97e9d4b4096
redacted-riri <sariah.echols.18@gmail.com>
2024-10-23 14:19:55 -0400

    Base features for Create New Event added

--- src/main.cpp
+++ src/main.cpp
@@ -7,10 +7,18 @@
 // - Documentation        https://dearimgui.com/docs (same as your local docs/ folder).
 // - Introduction, links and more at the top of imgui.cpp
 
+// Define the lowest year that the picker can select. In this example, '1970' is the Unix epoch.
+#define IMGUI_DATEPICKER_YEAR_MIN 1970
+// Define the highest year that the picker can select.
+#define IMGUI_DATEPICKER_YEAR_MAX 2500
+#include <ImGuiDatePicker.hpp>
+#include <chrono>
+
 #include "imgui.h"
 #include "imgui_impl_glfw.h"
 #include "imgui_impl_opengl3.h"
 #include <stdio.h>
+#include <string>
 #define GL_SILENCE_DEPRECATION
 #if defined(IMGUI_IMPL_OPENGL_ES2)
 #include <GLES2/gl2.h>
@@ -162,6 +170,7 @@ int main(int, char**)
         {
             static float f = 0.0f;
             static int counter = 0;
+            
 
             ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
 
@@ -176,6 +185,69 @@ int main(int, char**)
                 counter++;
             ImGui::SameLine();
             ImGui::Text("counter = %d", counter);
+
+            // Starting Create New modal button
+            static char eventName[128] = "";  // Buffer for event name
+            static char eventLocation[128] = "";    // Buffer for location
+            const char* hours[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"};
+            const char* minutes[60] = {};
+            static int currentHour = 1;
+            static int currentMinute = 0;
+            static int ampm = 0;
+            static int repeatable = 0;
+            if (ImGui::Button("New..."))
+                ImGui::OpenPopup("Stacked 1");
+            if (ImGui::BeginPopupModal("Stacked 1"))
+            {
+                ImGui::Text("Create New Event");
+                ImGui::Text("Event Name");
+                ImGui::InputText("##EventName", eventName, sizeof(eventName));
+                ImGui::Text("Location");
+                ImGui::InputText("##Location", eventLocation, sizeof(eventLocation));
+                std::chrono::system_clock::time_point now = std::chrono::system_clock::now();
+                std::time_t currentTime = std::chrono::system_clock::to_time_t(now);
+                tm t = *std::gmtime(&currentTime);
+                static tm setDate = t;
+                ImGui::Text("Date");
+                // Use the picker for date
+                if (ImGui::DatePicker("", setDate))
+                {
+                    // Perform some event whenever the date 't' is changed
+                }
+                // 
+                ImGui::Text("Time");
+                ImGui::PushItemWidth(40);
+                // Hours and minutes picker
+                ImGui::ListBox("Hour", &currentHour, hours, IM_ARRAYSIZE(hours), 4); ImGui::PushItemWidth(40); ImGui::SameLine();
+                for (int i = 0; i < 60; i++) {
+                    char* minuteStr = new char[3];
+                    snprintf(minuteStr, 3, "%02d", i);
+                    minutes[i] = minuteStr;
+                }
+                ImGui::ListBox("Minute", &currentMinute, minutes, IM_ARRAYSIZE(minutes), 4); ImGui::PushItemWidth(40); ImGui::SameLine();
+                // AM/PM Picker
+                ImGui::RadioButton("AM", &ampm, 0); ImGui::SameLine();
+                ImGui::RadioButton("PM", &ampm, 1);
+
+                // Repeat Event
+                ImGui::Text("Repeat");
+                ImGui::RadioButton("Never", &repeatable, 0); ImGui::SameLine();
+                ImGui::RadioButton("Daily", &repeatable, 1); ImGui::SameLine();
+                ImGui::RadioButton("Weekly", &repeatable, 2); ImGui::SameLine();
+                ImGui::RadioButton("Monthly", &repeatable, 3); ImGui::SameLine();
+                ImGui::RadioButton("Yearly", &repeatable, 4);
+
+                if (ImGui::Button("Create")) {
+                    // Put event on calendar
+
+                    // Close popup
+                    ImGui::CloseCurrentPopup();
+                }
+         
+                if (ImGui::Button("Close"))
+                    ImGui::CloseCurrentPopup();
+                ImGui::EndPopup();
+            }
 
             ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
             ImGui::End();
@@ -227,3 +299,6 @@ int main(int, char**)
 
     return 0;
 }
+
+
+
